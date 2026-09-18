7a7055033b5649e93495c3aed59a516528010b32
Jamie Piperberg <jamie.piperberg@gmail.com>
2026-09-18 09:19:03 -0400

    adjust tr since single quotes are required

--- SharedProcessors/TextStreamEditor.py
+++ SharedProcessors/TextStreamEditor.py
@@ -98,21 +98,17 @@ class TextStreamEditor(Processor):
         edited_string = ""
         # assemble command
         if len(sed_expression) > 0:
-            command = r"echo {0} |  sed '{1}'".format(source_string, sed_expression)
+            command = r"echo {0} | sed '{1}'".format(source_string, sed_expression)
             self.output("Preparing to execute command: {0}".format(command))
             edited_string = subprocess.run(command, capture_output=True, shell=True).stdout.decode().strip()
             self.output("Edited String is: {0}".format(edited_string))
+        else:
+            edited_string = source_string
         if len(tr_expression) > 0:
-            if len(edited_string) == 0:
-                command = r"echo {0} |  tr '{1}'".format(source_string, tr_expression)
-                self.output("Preparing to execute command: {0}".format(command))
-                edited_string = subprocess.run(command, capture_output=True, shell=True).stdout.decode().strip()
-                self.output("Edited String is: {0}".format(edited_string))
-            else:
-                command = r"echo {0} |  tr '{1}'".format(edited_string, tr_expression)
-                self.output("Preparing to execute command: {0}".format(command))
-                edited_string = subprocess.run(command, capture_output=True, shell=True).stdout.decode().strip()
-                self.output("Edited String is: {0}".format(edited_string))
+            command = r"echo {0} | tr {1}".format(edited_string, tr_expression)
+            self.output("Preparing to execute command: {0}".format(command))
+            edited_string = subprocess.run(command, capture_output=True, shell=True).stdout.decode().strip()
+            self.output("Edited String is: {0}".format(edited_string))
 
         self.env[output_var_name] = edited_string
 
